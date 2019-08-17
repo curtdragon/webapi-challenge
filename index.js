e7d5665ff9b0fc76c8e034ec4dce49edf84c2c98
curtdragon <tealturboser@gmail.com>
2019-08-16 23:20:37 -0500

    Add dotenv to deploy

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ I need this code, just don't know where, perhaps should make some middleware, do
 
 Go code!
 */
-
+require("dotenv").config()
 const server = require ("./server");
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
     console.log(`\n* Server Running on http://localhost:${port} *\n`);
